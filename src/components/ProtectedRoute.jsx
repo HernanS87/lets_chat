101db03f3caa787cf09ac360b3c8daf8c1b9d6cd
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-04-16 22:15:53 -0300

    Se comenzó a crear el sideBar

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,19 +1,19 @@
-import { useAuthContext } from '../context/AuthContext';
-import { HashLoader } from 'react-spinners';
-import { Navigate } from 'react-router-dom';
+import { useAuthContext } from "../context/AuthContext";
+import { HashLoader } from "react-spinners";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-	const { user, loading } = useAuthContext();
+  const { user, loading } = useAuthContext();
 
-	if (loading)
-		return (
-			<div className="min-h-screen bg-slate-800 flex justify-center items-center">
-				<HashLoader color={'#36d7b7'} size={100} className="h-screen mx-auto" />
-			</div>
-		);
-	if (!user) return <Navigate to="/login" />;
-
-	return <>{children}</>;
+  return loading ? (
+    <div className="min-h-screen bg-slate-800 flex justify-center items-center">
+      <HashLoader color={"#36d7b7"} size={100} className="h-screen mx-auto" />
+    </div>
+  ) : !user ? (
+    <Navigate to="/login" />
+  ) : (
+    <>{ children }</>
+  );
 };
 
 export default ProtectedRoute;
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -6,6 +6,7 @@ import Message from "../components/Message";
 import { HashLoader } from "react-spinners";
 import { Link } from "react-router-dom";
 import { useChatContext } from "../context/ChatContext";
+import sinImagen from "../assets/sinImagen.jpg";
 
 const Chat = () => {
   const [allMessages, setAllMessages] = useState([]);
@@ -142,17 +143,59 @@ const Chat = () => {
 
   if (!activeChannel)
     return (
-      <div className="h-[calc(100vh-80px)] flex justify-center items-center">
-        <h2 className="text-lg md:text-xl font-medium space-x-2">
-          <span>Selecciona o</span>
-          <Link
-            to="/create-channel"
-            className="text-cyan-500 hover:text-cyan-700 transition-all ease-in-out"
-          >
-            crea
-          </Link>
-          <span>un canal para comenzar a chatear</span>
-        </h2>
+      <div className="flex">
+        <section className="flex flex-col gap-3 relative items-center p-2 bg-gray-500 h-screen w-1/4">
+          <div className="bg-slate-800 p-1">
+            <input type="text" placeholder="buscador" />
+          </div>
+          <nav className="w-full absolute top-12 h-[calc(100vh-48px)] bg-slate-700 scrollbar-thin scroll-px-10 scrollbar-thumb-cyan-500 dark:scrollbar-track-gray-900 scrollbar-track-gray-200">
+            <ul className=" ">
+              <li className="border-b-2 border-slate-900 px-1 py-2">
+                <div className="flex gap-2 items-center">
+                  <img
+                    src={sinImagen}
+                    alt="sinPic"
+                    className="w-10 aspect-square rounded-full"
+                  />
+                  <span>Canal Topo</span>
+                </div>{" "}
+              </li>
+              <li className="border-b-2 border-slate-900 px-1 py-2">
+                <div className="flex gap-2 items-center">
+                  <img
+                    src={sinImagen}
+                    alt="sinPic"
+                    className="w-10 aspect-square rounded-full"
+                  />
+                  <span>Canal Topo</span>
+                </div>{" "}
+              </li>
+              <li className="border-b-2 border-slate-900 px-1 py-2">
+                <div className="flex gap-2 items-center">
+                  <img
+                    src={sinImagen}
+                    alt="sinPic"
+                    className="w-10 aspect-square rounded-full"
+                  />
+                  <span>Canal Topo</span>
+                </div>{" "}
+              </li>
+            </ul>
+          </nav>
+        </section>
+        <div className=" w-full h-[calc(100vh-80px)] flex justify-center items-center">
+          <h2 className="text-lg md:text-xl font-medium space-x-2">
+            <span>Selecciona o</span>
+            <Link
+              to="/create-channel"
+              className="text-cyan-500 hover:text-cyan-700 transition-all ease-in-out"
+            >
+              crea
+            </Link>
+            <span>un canal para comenzar a chatear</span>
+          </h2>
+        </div>
+        <img src="" alt="" />
       </div>
     );
 
